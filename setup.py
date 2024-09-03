4938f4bac67f927bc283e04b63ead5c57467fd33
ImNotOssy <imnotossy420@gmail.com>
2024-09-03 15:00:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="bbae_invest_api",
     version="0.1.0",
-    packages=find_packages(),
+    description="Unofficial BBAE Invest API written in Python Requests",
+    url="https://github.com/ImNotOssy/BBAE_investing_API",
+    author="Oswaldo Romero",
+    packages=["bbae_invest_api"],
     install_requires=[
         "pillow",
         "python-dotenv",
