e9bb9aa7681e7ae6518e83625bcfa4cfbe3cfd44
ImNotOssy <imnotossy420@gmail.com>
2024-09-05 15:46:40 -0500

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bbae_invest_api",
-    version="0.1.0",
+    version="0.1.1",
     description="Unofficial BBAE Invest API written in Python Requests",
     url="https://github.com/ImNotOssy/BBAE_investing_API",
     author="Oswaldo Romero",
