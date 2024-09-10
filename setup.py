218a908e64bc02a57ab86d4a37a76e31c028b1ae
ImNotOssy <imnotossy420@gmail.com>
2024-09-10 03:20:12 -0500

    Added even more style changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bbae_invest_api",
-    version="0.1.2",
+    version="0.1.3",
     description="Unofficial BBAE Invest API written in Python Requests",
     url="https://github.com/ImNotOssy/BBAE_investing_API",
     author="Oswaldo Romero",
