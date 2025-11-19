1e1e9bd3fd59a9599ea2ad9bfdf7cd166121b22a
ImNotOssy <85385882+ImNotOssy@users.noreply.github.com>
2025-11-19 09:08:31 -0600

    Bump version to 0.1.4 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bbae_invest_api",
-    version="0.1.3",
+    version="0.1.4",
     description="Unofficial BBAE Invest API written in Python Requests",
     url="https://github.com/ImNotOssy/BBAE_investing_API",
     author="Oswaldo Romero",
@@ -13,3 +13,4 @@ setup(
         "requests",
     ],
 )
+
